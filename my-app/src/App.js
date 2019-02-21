df1f16c22ad3d24744a787c6e3f09f76e8f381bc
Hany Rostom <hanyrostom@gmail.com>
2019-02-21 11:31:24 -0500

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+        <div>
+           <h1>taskr</h1>
+           <h1>taskr</h1>
+        </div>
+
+        <div id="main">
+
+        </div>
+         
+
+        
+      </div>
+    );
+  }
+}
+
+export default App;
